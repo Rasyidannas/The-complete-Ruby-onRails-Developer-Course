27761eb37d38dbd3f9913caa3564261a8287f4a9
rasyidannas <rasshit.dsgn@gmail.com>
2024-09-11 08:29:31 +0700

    4/100. DRY code - refactoringand partials.mp4

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
     # byebug
-    @article = Article.find(params[:id])
+
   end
 
   def index
@@ -14,13 +16,12 @@ class ArticlesController < ApplicationController
 
   def edit 
     # byebug
-    @article = Article.find(params[:id])
   end
 
   def create
     # render plain: params[:article] #this will render into browser after save
 
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -33,8 +34,8 @@ class ArticlesController < ApplicationController
 
   def update
     # byebug
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+
+    if @article.update(article_params)
         flash[:notice] = "Article was updated successfully"
         redirect_to @article
     else
@@ -43,9 +44,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted."
     redirect_to articles_path
+  end
+
+  private 
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
