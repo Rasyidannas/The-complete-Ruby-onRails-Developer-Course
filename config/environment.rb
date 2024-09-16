518ad9ee289e5acbc178ebbe697ec996bc6e9d76
rasyidannas <rasshit.dsgn@gmail.com>
2024-09-16 10:10:53 +0700

    5/118. Style validation and flash messages.mp4

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Remove the default field error proc
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
