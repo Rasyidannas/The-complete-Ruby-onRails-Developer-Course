a9276ca8c4024ca0f2179ac5db5904cf5a6c326e
rasyidannas <rasshit.dsgn@gmail.com>
2024-11-25 08:51:55 +0700

    7/197. Many to many association back end implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_16_152452) do
+ActiveRecord::Schema.define(version: 2024_11_21_044930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
