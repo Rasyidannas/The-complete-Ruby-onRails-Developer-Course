b2333b02c59d8ddc0e402f5e4cb683a65639750d
rasyidannas <rasshit.dsgn@gmail.com>
2024-12-26 11:46:04 +0700

    8/22. Task 4: Build user resource

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'webpacker', '~> 4.0'
 gem 'jbuilder', '~> 2.7'
 gem 'semantic-ui-sass'
 gem 'jquery-rails'
+gem 'bcrypt', '~> 3.1.7'
+gem 'hirb'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Rasyid", password: "1234567890")
+User.create(username: "Ciko", password: "1234567890")
+
